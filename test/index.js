76e752a18f66274666a0bf970d4a187efaefa05c
Gil Pedersen <git@gpost.dk>
2020-09-15 16:12:53 +0200

    Use new hls-segment-reader and m3u8parse

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const Url = require('url');
 
 const Code = require('@hapi/code');
 const HlsRecord = require('..');
-const HlsSegmentReader = require('hls-segment-reader');
+const { createSimpleReader } = require('hls-segment-reader');
 const Lab = require('@hapi/lab');
 
 
@@ -22,7 +22,7 @@ describe('HlsStreamRecorder', () => {
 
     it('creates new instances', () => {
 
-        const reader = new HlsSegmentReader(Url.pathToFileURL(Path.join(internals.fixtureDir, '500.m3u8')), { withData: true });
+        const reader = createSimpleReader(Url.pathToFileURL(Path.join(internals.fixtureDir, '500.m3u8')), { withData: true });
         const recorder = HlsRecord(reader, 's3://not-really-there/ingest');
 
         expect(recorder).to.be.instanceof(HlsRecord.HlsStreamRecorder);
