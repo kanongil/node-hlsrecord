df3d8995b5925472d688131a7f50d3b5bb1c891b
Gil Pedersen <git@gpost.dk>
2021-05-18 11:54:26 +0200

    Handle premature close

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const Url = require('url');
 
 const Code = require('@hapi/code');
 const HlsRecord = require('..');
-const { createSimpleReader } = require('hls-segment-reader');
+const { HlsSegmentReader } = require('hls-segment-reader');
 const Lab = require('@hapi/lab');
 
 
@@ -20,13 +20,27 @@ const { expect } = Code;
 
 describe('HlsStreamRecorder', () => {
 
-    it('creates new instances', () => {
+    it('handles immediate reader.destroy()', async () => {
 
-        const reader = createSimpleReader(Url.pathToFileURL(Path.join(internals.fixtureDir, '500.m3u8')), { withData: true });
+        const reader = new HlsSegmentReader(Url.pathToFileURL(Path.join(internals.fixtureDir, '500.m3u8')), { withData: true });
         const recorder = HlsRecord(reader, 's3://not-really-there/ingest');
 
         expect(recorder).to.be.instanceof(HlsRecord.HlsStreamRecorder);
 
-        reader.abort();
+        reader.destroy();
+
+        await expect(recorder.completed()).to.reject('premature close');
+    });
+
+    it('handles immediate recorder.destroy() with error', async () => {
+
+        const reader = new HlsSegmentReader(Url.pathToFileURL(Path.join(internals.fixtureDir, '500.m3u8')), { withData: true });
+        const recorder = HlsRecord(reader, 's3://not-really-there/ingest');
+
+        expect(recorder).to.be.instanceof(HlsRecord.HlsStreamRecorder);
+
+        reader.destroy(new Error('aborted'));
+
+        await expect(recorder.completed()).to.reject('aborted');
     });
 });
